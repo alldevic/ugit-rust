8ed74e3cd16552cef485c1b7da557a4aede9f8ea
Nikolay Bely <belyynikolay@gmail.com>
2023-12-29 11:38:20 +0000

    test

--- src/data.rs
+++ src/data.rs
@@ -12,12 +12,20 @@ pub fn init() -> std::io::Result<()> {
     Ok(())
 }
 
-pub fn hash_object(filename: String) -> std::io::Result<String> {
+pub fn hash_object(filename: String, file_type: Option<&str>) -> std::io::Result<String> {
+    let file_type = file_type.unwrap_or("blob");
+
     let mut file = fs::File::open(&filename)?;
+
     let mut hasher = Sha1::new();
+    let mut contents = String::new();
+    
     let _ = io::copy(&mut file, &mut hasher)?;
     let hash  = hex::encode(hasher.finalize());
+
+    file.read_to_string(&mut contents);
     
+    let mut obj = [file_type.as_bytes(), b"\x00", ].concat();
     // let mut object_file = fs::File::create(format!("{}/objects/{}", GIT_DIR, hash))?;
 
     // let mut chunk = file.take(100);
@@ -29,11 +37,11 @@ pub fn hash_object(filename: String) -> std::io::Result<String> {
     return Ok(hash);
 }
 
-pub fn get_object(object: String) -> std::io::Result<String>  {
+pub fn get_object(object: String, expected: Option<&str>) -> std::io::Result<String> {
+    let expected = expected.unwrap_or("blob");
     let path = format!("{}/{}", OBJECTS_DIR, object);
 
-    let contents = fs::read_to_string(path)
-        .expect("Should have been able to read the file");
+    let file = fs::read(path);
 
     Ok(contents)
 }
